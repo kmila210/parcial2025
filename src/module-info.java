54250a72c81c1173599ddb30952439f543636e36
Camila <cq239910@gmail.com>
2025-05-08 15:47:37 -0300

    primer commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module parcialTabone2025 {
+}
---
+++ src/parcialTabone2025/Main.java
@@ -0,0 +1,9 @@
+package parcialTabone2025;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
